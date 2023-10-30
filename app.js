58f0a8133c0a1007bd385b81b82edea5c4007265
RodrigoSaporiti <rodrigosaporiti6949@gmail.com>
2023-10-30 20:10:14 -0300

    1

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+const booksData = fs.readFileSync('books.json');
+const books = JSON.parse(booksData);
+app.get('/books', (req, res) => {
+res.json(books);
+});
+app.post('/books', (req, res) => {
+const newBook = req.body;
+// Completa la lógica para agregar el nuevo libro a la lista de
+libros
+// y guardar la lista actualizada en 'books.json'.
+fs.writeFileSync('books.json', JSON.stringify(books, null, 2));
+res.status(201).json(newBook);
+});
+app.put('/books/:id', (req, res) => {
+const bookId = req.params.id;
+const updatedBook = req.body;
+// Completa la lógica para actualizar el libro con el indice
+proporcionado
+// y guardar la lista actualizada en 'books.json'.
+fs.writeFileSync('books.json', JSON.stringify(books, null, 2));
+res.json(updatedBook);
+});
+app.delete('/books/:id', (req, res) => {
+const bookId = req.params.id;
+// Completa la lógica para eliminar el libro con el ID
+proporcionado
+// y guardar la lista actualizada en 'books.json'.
+fs.writeFileSync('books.json', JSON.stringify(books, null, 2));
+res.json(deletedBook);
+});
+app.listen(port, () => {
+console.log(`http://localhost:${port}`);
+
+});
